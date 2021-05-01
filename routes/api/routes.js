9ee2c73b0ffd68a5af05db1256627023c87478ed
Noor Abdikadir <nooromar305@gmail.com>
2021-05-01 06:16:36 -0700

    routes creating sign up form

--- routes/api/routes.js
+++ routes/api/routes.js
@@ -1,10 +1,17 @@
+const express = require('express');
 const router = require("express").Router();
+const signupForm = require('../models/signupForm');
 const movieRoutes = require("./movie");
 
 // Post routes
 router.use("/movie", movieRoutes);
 
 router.post('signup', (request, response) =>{
-    response.send('send');
+    const signedupUser = new signupFormCopy({
+        fullname:requestbody.fullname,
+        username:requestbody.username,
+        email:requestbody.email,
+        password:requestbody.password,
+    })
 })
 module.exports = router;
