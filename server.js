730edcd22a792ca66c041dfc828d083170114e52
Noor Abdikadir <nooromar305@gmail.com>
2021-05-01 11:11:29 -0700

    user.js

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require("express");
+
 const mongoose = require("mongoose");
-const dotenv = require('dotenv');
 const routes = require("./routes");
-const routesUrls = require("./routes/routes");
-const cors = require('cors')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -16,16 +14,11 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+// Add routes, both API and view
+app.use(routes);
 
-dotenv.config()
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/movieDB");
-mongoose.connect(process.env.DATABASE_ACCESS, () => console.log("Database connected"))
-
-// Add routes, both API and view
-app.use(express.json())
-app.use(cors())
-app.use('/app', routesUrls);
 
 // Start the API server
 app.listen(PORT, function () {
