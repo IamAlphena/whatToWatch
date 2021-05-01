c83629e8aa5fcf823d731d3b4ef7a174f7c5dfb8
Noor Abdikadir <nooromar305@gmail.com>
2021-05-01 06:11:52 -0700

    added routes & signup model

--- routes/api/routes.js
+++ routes/api/routes.js
@@ -4,4 +4,7 @@ const movieRoutes = require("./movie");
 // Post routes
 router.use("/movie", movieRoutes);
 
+router.post('signup', (request, response) =>{
+    response.send('send');
+})
 module.exports = router;
