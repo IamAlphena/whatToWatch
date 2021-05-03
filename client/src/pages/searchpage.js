011a5263a339ab94918833a585e518dc28c41f74
unname666 <78314985+unname666@users.noreply.github.com>
2021-05-03 14:17:57 -0700

    npm changes and minor changes

--- client/src/pages/searchpage.js
+++ client/src/pages/searchpage.js
@@ -1,17 +1,49 @@
-import React from 'react'
-import SearchBar from "../components/Searchbar"
-import MovieCard from "../components/Cards"
+import React, { useEffect, useState } from "react";
+import MovieCard from "../components/Cards";
+import axios from "axios";
 
-function SearchPage() {
-    return(<>
-    <SearchBar/>
-    
-    <div class="cardContainer">
+const SearchPage = () => {
+  const [title, setTitle] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const getMovie = async (title) => {
+    const apiKey = process.env.REACT_APP_API_KEY;
+    const url = `https://api.themoviedb.org/3/search/multi/?api_key=${apiKey}&language=en-US&query=${title}&page=1&include_adult=false`;
+
+    let movieData = await axios.get(url);
+    console.log(movieData);
+  };
+
+  useEffect(() => {
+    getMovie(title);
+  });
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Search"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        {/* onclick on button */}
+
+        <div>
+          <input type="checkbox" id="tv" name="tv" />
+          <label htmlFor="tv">Tv Show</label>
+          <input type="checkbox" id="movie" name="movie" />
+          <label htmlFor="movie">Movie</label>
+        </div>
+      </form>
+      <h5>this is the sreach title{title}</h5>
+      <div className="cardContainer">
         <MovieCard />
-
-    </div>
+      </div>
     </>
-    )
-}
+  );
+};
 
 export default SearchPage;
