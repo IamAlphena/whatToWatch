f6faf1d4dedc2798e8f1d9e14ff7beaa3bcbe7e9
Alphena <alphena@gmail.com>
2021-05-03 11:26:51 -0700

    added card components

--- client/src/pages/searchpage.js
+++ client/src/pages/searchpage.js
@@ -1,23 +1,14 @@
 import React from 'react'
 import SearchBar from "../components/Searchbar"
+import MovieCard from "../components/Cards"
 
 function SearchPage() {
     return(<>
-<SearchBar/>
+    <SearchBar/>
     
-<div class="cardContainer">
-        <div class="card">
-            <img src="https://pics.filmaffinity.com/Jumanji_The_Next_Level-407262058-mmed.jpg"
-                alt="Jumanji The Next Level" />
-        </div>
-        <div class="card">
-            <img src="https://m.media-amazon.com/images/M/MV5BODQ0NDhjYWItYTMxZi00NTk2LWIzNDEtOWZiYWYxZjc2MTgxXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg"
-                alt="Jumanji Welcome to the Jungle" />
-        </div>
-        <div class="card">
-            <img src="https://img01.mgo-images.com/image/thumbnail/v2/content/1MVfbb95116ae0e6bd2d8445ef756c26b77.jpeg"
-                alt="Jumanji" />
-        </div>
+    <div class="cardContainer">
+        <MovieCard />
+
     </div>
     </>
     )
