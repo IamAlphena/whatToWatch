1b374ba2fc37a1a6f04b2353f6f8842dd2b4ba78
Alphena <alphena@gmail.com>
2021-05-03 01:29:00 -0700

    Added first page of react element

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
-import Header from './components/header'
-import Footer from './components/footer'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from './components/header';
+import Footer from './components/footer';
+import Search from './pages/searchpage';
 
 function App() {
   return (
-    <> 
-    <Header /> 
-    <div>
-      Hello world!!
-    </div>
-    <Footer />
-    </>
+    <Router>
+      <div>
+      <Header />
+      {/* Nav here */}
+        <Switch>
+          <Route exact path='/' component={Search} />
+        </Switch>
+      <Footer />
+      </div>
+    </Router>
 
   );
 }
---
+++ client/src/pages/searchpage.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function SearchPage() {
+    return(
+<div class="cardContainer">
+        <div class="card">
+            <img src="https://pics.filmaffinity.com/Jumanji_The_Next_Level-407262058-mmed.jpg"
+                alt="Jumanji The Next Level" />
+        </div>
+        <div class="card">
+            <img src="https://m.media-amazon.com/images/M/MV5BODQ0NDhjYWItYTMxZi00NTk2LWIzNDEtOWZiYWYxZjc2MTgxXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg"
+                alt="Jumanji Welcome to the Jungle" />
+        </div>
+        <div class="card">
+            <img src="https://img01.mgo-images.com/image/thumbnail/v2/content/1MVfbb95116ae0e6bd2d8445ef756c26b77.jpeg"
+                alt="Jumanji" />
+        </div>
+    </div>
+    )
+}
+
+export default SearchPage;
