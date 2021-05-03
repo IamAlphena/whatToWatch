ce99a8d154c4ad89dc3956a97789975cdfbfc02e
unname666 <78314985+unname666@users.noreply.github.com>
2021-05-03 15:09:34 -0700

    navbar done

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
+
 function Header() {
     return (
         <header>
         <p>What to Watch</p>
-        <nav> This is the save spot for the nav info</nav>
     </header>
     )
 }
