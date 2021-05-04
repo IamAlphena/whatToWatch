d88d9779e9b0c3c10c76c0d4efca46ecf5fb83f1
unname666 <78314985+unname666@users.noreply.github.com>
2021-05-04 10:36:23 -0700

    got the api to call on button click

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,7 +1,15 @@
 import axios from "axios";
 
-function apiCall (){
-    
-}
+const apiKey = process.env.REACT_APP_API_KEY;
 
-export default apiCall
+const API = {
+    getMovie: async (title) => {
+    const url = `https://api.themoviedb.org/3/search/multi/?api_key=${apiKey}&language=en-US&query=${title}&page=1&include_adult=false`;
+
+    let movieData = await axios.get(url);
+    console.log(movieData);
+    // return movieData
+  },
+};
+
+export default API;
