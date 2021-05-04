7514108becd55d93d10dfa0212e9f9c2eb3a8bc3
Alphena <alphena@gmail.com>
2021-05-04 13:43:09 -0700

    added titles and button

--- client/src/components/Cards/index.js
+++ client/src/components/Cards/index.js
@@ -3,11 +3,13 @@ import { Card } from 'react-bulma-components';
 
 function MovieCard(props) {
     return (
-       <Card style={{ width: 150, margin: 'auto'}}>
+       <Card style={{ width: 150, margin: 10, height: 'auto' }}>
             <Card.Image
         size="3by4"
         src={props.image}
          /> 
+         <Card.Header.Title style={{height: 'auto'}}>{props.title}</Card.Header.Title>
+         <button className="is-primary">Save</button>
          </Card>            
         )
 }
